56b4dd0502c1acc50f14b95ab98e4eb5dc162b5f
ppvaladao <pedrim54@hotmail.com>
2020-08-01 19:10:10 -0300

    go

--- app.js
+++ app.js
@@ -8,12 +8,12 @@ app.listen(80);
 //ajeitar logs na db e mostrar no site
 //colocar esquema de login para usar botões. //mais difícil
 
-// const loop1 = () => huntedss().finally(loop1);
-// const loop2 = () => tracker_exp().finally(loop2);
-// const loop3 = () => onlines_update().finally(loop3);
-// loop1()
-// loop2()
-// loop3()
+const loop1 = () => huntedss().finally(loop1);
+const loop2 = () => tracker_exp().finally(loop2);
+const loop3 = () => onlines_update().finally(loop3);
+loop1()
+loop2()
+loop3()
 
 
 
