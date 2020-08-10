7462e9c130fe9696d169a19825a5828a067aa0f2
root <root@ip-172-31-6-248.sa-east-1.compute.internal>
2020-08-10 12:21:02 +0000

    go

---
+++ utorionData/index.js
@@ -0,0 +1,33 @@
+const puppeteer = require('puppeteer');
+const url_names = 'https://www.utorion.com/index.php?subtopic=whoisonline';
+const fs = require('fs')
+
+
+async function onlines_now() {
+    const browser = await puppeteer.launch({args: ['--no-sandbox', '--disable-setuid-sandbox'], waitUntil: 'networkidle0'});
+    const page = await browser.newPage();
+    await page.goto(url_names);
+    await page.waitForSelector('a[href*="?subtopic=characters"]');
+    on_now = await page.$$eval('.Table2 tr:not(:first-child)', ($els) => {
+        return $els.map(($el) => {
+            const name = $el.querySelector('td:nth-child(1) a').innerHTML;
+            const level = $el.querySelector('td:nth-child(2)').innerHTML;
+            const vocation = $el.querySelector('td:nth-child(3)').innerHTML.split('&nbsp;').join(' ');
+            return {
+                name,
+                level,
+                vocation
+            };
+        });
+    });
+    
+    await browser.close();
+    fs.writeFileSync(__dirname + "/lastOnlines.json", JSON.stringify(on_now))
+    
+};
+
+
+module.exports ={
+    onlines_now
+  
+};
